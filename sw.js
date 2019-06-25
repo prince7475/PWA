40b0235dfc3ccc45fd234e3744cf9a97646252d7
Prince Donkor <prince7475>
2019-06-24 19:25:48 -0500

    updated

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = 'site-static-v2'
+const staticCacheName = 'site-static-v3'
+const dynamicCacheName = 'site-dynamic-v4'
 const assets = [
     '/',
     '/index.html',
@@ -8,6 +9,7 @@ const assets = [
     '/css/styles.css',
     '/css/materialize.min.css',
     '/img/dish.png',
+    '/pages/fallback.html',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
     'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2'
 ];
@@ -30,7 +32,7 @@ self.addEventListener('activate', evt => {
         caches.keys().then( keys => {
             // console.log(keys)
             return Promise.all(keys
-                .filter(key => key !== staticCacheName)
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
                 .map(key => caches.delete(key))
                 )
         })
@@ -39,13 +41,19 @@ self.addEventListener('activate', evt => {
 })
 
 
-// Fetch Events 
 self.addEventListener('fetch', evt => {
     evt.respondWith(
         caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request)
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url,fetchRes.clone());
+                    return fetchRes
+                })
+            })
+        }).catch(e => {
+            if(evt.request.url.indexOf('.html') >= 0){
+                return caches.match('/pages/fallback.html')
+            }
         })
     )
-
-
 })
